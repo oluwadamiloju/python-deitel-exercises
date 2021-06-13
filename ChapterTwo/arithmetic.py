1f99fe9ce91eeea39c209f63c48cc83f25f0db7f
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-06-13 22:37:59 +0100

    2.4

---
+++ ChapterTwo/arithmetic.py
@@ -0,0 +1,44 @@
+first_number = 27.5
+second_number = 2
+
+
+def addition():
+    return first_number + second_number
+
+
+print(addition())
+
+
+def subtraction():
+    return first_number - second_number
+
+
+print(subtraction())
+
+
+def multiplication():
+    return first_number * second_number
+
+
+print(multiplication())
+
+
+def true_division():
+    return first_number / second_number
+
+
+print(true_division())
+
+
+def floor_division():
+    return first_number // second_number
+
+
+print(floor_division())
+
+
+def exponentiation():
+    return first_number // second_number
+
+
+print(exponentiation())
