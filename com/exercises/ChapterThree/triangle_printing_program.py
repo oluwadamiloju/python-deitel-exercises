ceced22b1ab86eaccc9b49399ff51a8d4aaa8dd8
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-24 16:09:56 +0100

    printing different right-angled triangles

---
+++ com/exercises/ChapterThree/triangle_printing_program.py
@@ -0,0 +1,43 @@
+def first_triangle():
+    for line in range(1, 11):
+        for stars in range(1, line):
+            print('*', end="")
+        print("")
+
+
+print(first_triangle())
+
+
+def second_triangle():
+    for line in range(1, 11):
+        for stars in range(10, line, -1):
+            print('*', end="")
+        print()
+
+
+print(second_triangle())
+
+
+def third_triangle():
+    for line in range(1, 11):
+        for spaces in range(1, line):
+            print(" ", end="")
+        for stars in range(10, line, -1):
+            print("*", end="")
+
+        print()
+
+
+print(third_triangle())
+
+
+def fourth_triangle():
+    for line in range(1, 11):
+        for spaces in range(9, line -1, -1):
+            print(" ", end="")
+        for stars in range(1, line):
+            print("*", end="")
+        print()
+
+
+print(fourth_triangle())
