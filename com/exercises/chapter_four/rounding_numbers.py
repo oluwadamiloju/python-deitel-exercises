5e48453f20d874faeec2a620b2747c2edebcd41d
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-09-28 15:22:02 +0100

    4.8

---
+++ com/exercises/chapter_four/rounding_numbers.py
@@ -0,0 +1,17 @@
+# 4.8 (Rounding Numbers) Investigate built-in function round at
+# https://docs.python.org/3/library/functions.html#round
+# then use it to round the float value 13.56449 to the nearest integer,
+# tenths, hundredths and thousandths positions.
+
+
+# to the nearest integer
+print(round(13.56449))
+
+# to the nearest tenth
+print(round(13.56449, 1))
+
+# to the nearest hundredth
+print(round(13.56449, 2))
+
+# to the nearest thousandth
+print(round(13.56449, 3))
